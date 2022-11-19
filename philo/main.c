67edfb9a6596cbdd8b46ec548ef8dbb94e302820
Daniela Sanchez <danisanc@c2s18d112.42wolfsburg.de>
2022-11-19 20:20:58 +0100

    double line

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: danisanc <danisanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 11:41:09 by danisanc          #+#    #+#             */
-/*   Updated: 2022/11/19 18:26:00 by danisanc         ###   ########.fr       */
+/*   Updated: 2022/11/19 20:20:13 by danisanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int	main(int argc, char **argv)
 	t_rules	data;
 
 	check_input(argv, argc);
-	data.start_time = get_time(0);
 	enter_data(argv, &data);
 	init_data(&data);
 	init_threads(&data);
